e88834ecfceaae4e9eaec2e84ef69ce0ddc353f9
dmoisa971 <david_moisa@hotmail.com>
2016-08-30 17:00:10 +0200

    Modification script PowerShell

--- DemoDeploiementSQL/Deploy-AzureResourceGroup.ps1
+++ DemoDeploiementSQL/Deploy-AzureResourceGroup.ps1
@@ -1,119 +1,41 @@
-﻿#Requires -Version 3.0
-#Requires -Module AzureRM.Resources
-#Requires -Module Azure.Storage
+﻿#Définition des variables utilisée pour générer le groupe de ressources
+#et pour ensuite déployer le template avec le fichier de paramètre
+$localisation = 'West Europe'
+$resourceGroupName = 'testdeploiementarmpowershell'
+$deploymentName = 'testdeploiement'
+$keyVaultName = "testdeploiementvaultdmo"
+$keyVaultSqlAdminPasswordSecretName = "sqlPassword"
+$serverPassword = "motDePasse2016!Admin#"   
+$templateFilePath = 'D:\Solutions\Azure\ARM\DemoAzureResourceGroup\DemoDeploiementSQL\Templates\azuredeploy.json'
+$templateParametersFilePath = 'D:\Solutions\Azure\ARM\DemoAzureResourceGroup\DemoDeploiementSQL\Templates\azuredeploy.parameters.json'
 
-Param(
-    [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation,
-    [string] $ResourceGroupName = 'DemoAzureResourceGroup',
-    [switch] $UploadArtifacts,
-    [string] $StorageAccountName,
-    [string] $StorageContainerName = $ResourceGroupName.ToLowerInvariant() + '-stageartifacts',
-    [string] $TemplateFile = 'Templates\azuredeploy.json',
-    [string] $TemplateParametersFile = 'Templates\azuredeploy.parameters.json',
-    [string] $ArtifactStagingDirectory = '.',
-    [string] $DSCSourceFolder = 'DSC'
-)
-
-Import-Module Azure -ErrorAction SilentlyContinue
-
-try {
-    [Microsoft.Azure.Common.Authentication.AzureSession]::ClientFactory.AddUserAgent("VSAzureTools-$UI$($host.name)".replace(" ","_"), "2.9.1")
-} catch { }
-
-Set-StrictMode -Version 3
-
-$OptionalParameters = New-Object -TypeName Hashtable
-$TemplateFile = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, $TemplateFile))
-$TemplateParametersFile = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, $TemplateParametersFile))
-
-if ($UploadArtifacts) {
-    # Convert relative paths to absolute paths if needed
-    $ArtifactStagingDirectory = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, $ArtifactStagingDirectory))
-    $DSCSourceFolder = [System.IO.Path]::GetFullPath([System.IO.Path]::Combine($PSScriptRoot, $DSCSourceFolder))
-
-    Set-Variable ArtifactsLocationName '_artifactsLocation' -Option ReadOnly -Force
-    Set-Variable ArtifactsLocationSasTokenName '_artifactsLocationSasToken' -Option ReadOnly -Force
-
-    $OptionalParameters.Add($ArtifactsLocationName, $null)
-    $OptionalParameters.Add($ArtifactsLocationSasTokenName, $null)
-
-    # Parse the parameter file and update the values of artifacts location and artifacts location SAS token if they are present
-    $JsonContent = Get-Content $TemplateParametersFile -Raw | ConvertFrom-Json
-    $JsonParameters = $JsonContent | Get-Member -Type NoteProperty | Where-Object {$_.Name -eq "parameters"}
-
-    if ($JsonParameters -eq $null) {
-        $JsonParameters = $JsonContent
-    }
-    else {
-        $JsonParameters = $JsonContent.parameters
-    }
-
-    $JsonParameters | Get-Member -Type NoteProperty | ForEach-Object {
-        $ParameterValue = $JsonParameters | Select-Object -ExpandProperty $_.Name
-
-        if ($_.Name -eq $ArtifactsLocationName -or $_.Name -eq $ArtifactsLocationSasTokenName) {
-            $OptionalParameters[$_.Name] = $ParameterValue.value
-        }
-    }
-
-    # Create DSC configuration archive
-    if (Test-Path $DSCSourceFolder) {
-        $DSCSourceFilePaths = @(Get-ChildItem $DSCSourceFolder -File -Filter "*.ps1" | ForEach-Object -Process {$_.FullName})
-        foreach ($DSCSourceFilePath in $DSCSourceFilePaths) {
-            $DSCArchiveFilePath = $DSCSourceFilePath.Substring(0, $DSCSourceFilePath.Length - 4) + ".zip"
-            Publish-AzureRmVMDscConfiguration $DSCSourceFilePath -OutputArchivePath $DSCArchiveFilePath -Force -Verbose
-        }
-    }
-
-    # Create a storage account name if none was provided
-    if($StorageAccountName -eq "") {
-        $subscriptionId = ((Get-AzureRmContext).Subscription.SubscriptionId).Replace('-', '').substring(0, 19)
-        $StorageAccountName = "stage$subscriptionId"
-    }
-
-    $StorageAccount = (Get-AzureRmStorageAccount | Where-Object{$_.StorageAccountName -eq $StorageAccountName})
-
-    # Create the storage account if it doesn't already exist
-    if($StorageAccount -eq $null){
-        $StorageResourceGroupName = "ARM_Deploy_Staging"
-        New-AzureRmResourceGroup -Location "$ResourceGroupLocation" -Name $StorageResourceGroupName -Force
-        $StorageAccount = New-AzureRmStorageAccount -StorageAccountName $StorageAccountName -Type 'Standard_LRS' -ResourceGroupName $StorageResourceGroupName -Location "$ResourceGroupLocation"
-    }
-
-    $StorageAccountContext = (Get-AzureRmStorageAccount | Where-Object{$_.StorageAccountName -eq $StorageAccountName}).Context
-
-    # Generate the value for artifacts location if it is not provided in the parameter file
-    $ArtifactsLocation = $OptionalParameters[$ArtifactsLocationName]
-    if ($ArtifactsLocation -eq $null) {
-        $ArtifactsLocation = $StorageAccountContext.BlobEndPoint + $StorageContainerName
-        $OptionalParameters[$ArtifactsLocationName] = $ArtifactsLocation
-    }
-
-    # Copy files from the local storage staging location to the storage account container
-    New-AzureStorageContainer -Name $StorageContainerName -Context $StorageAccountContext -Permission Container -ErrorAction SilentlyContinue *>&1
-
-    $ArtifactFilePaths = Get-ChildItem $ArtifactStagingDirectory -Recurse -File | ForEach-Object -Process {$_.FullName}
-    foreach ($SourcePath in $ArtifactFilePaths) {
-        $BlobName = $SourcePath.Substring($ArtifactStagingDirectory.length + 1)
-        Set-AzureStorageBlobContent -File $SourcePath -Blob $BlobName -Container $StorageContainerName -Context $StorageAccountContext -Force
-    }
-
-    # Generate the value for artifacts location SAS token if it is not provided in the parameter file
-    $ArtifactsLocationSasToken = $OptionalParameters[$ArtifactsLocationSasTokenName]
-    if ($ArtifactsLocationSasToken -eq $null) {
-        # Create a SAS token for the storage container - this gives temporary read-only access to the container
-        $ArtifactsLocationSasToken = New-AzureStorageContainerSASToken -Container $StorageContainerName -Context $StorageAccountContext -Permission r -ExpiryTime (Get-Date).AddHours(4)
-        $ArtifactsLocationSasToken = ConvertTo-SecureString $ArtifactsLocationSasToken -AsPlainText -Force
-        $OptionalParameters[$ArtifactsLocationSasTokenName] = $ArtifactsLocationSasToken
-    }
+#Creation du groupe de ressources
+{
+    New-AzureRmResourceGroup -Name $resourceGroupName -Location $localisation  -Verbose -Force
 }
 
-# Create or update the resource group using the specified template file and template parameters file
-New-AzureRmResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation -Verbose -Force -ErrorAction Stop
+#Creation du KeyVault pour la gestion du mot de passe de l'administrateur du serveur
+{
+    New-AzureRmKeyVault -VaultName $keyVaultName -ResourceGroupName $resourceGroupName -Location $localisation -EnabledForDeployment -EnabledForTemplateDeployment
+}
 
-New-AzureRmResourceGroupDeployment -Name ((Get-ChildItem $TemplateFile).BaseName + '-' + ((Get-Date).ToUniversalTime()).ToString('MMdd-HHmm')) `
-                                   -ResourceGroupName $ResourceGroupName `
-                                   -TemplateFile $TemplateFile `
-                                   -TemplateParameterFile $TemplateParametersFile `
-                                   @OptionalParameters `
-                                   -Force -Verbose
+#Creation d'une règle d'accès au container Key Vault (n'est nécessaire que dans le cas ou vous avez une sortie vous informatant q'aucune policy n'est mise en place
+#au niveau du container Key Vault
+{
+    Set-AzureRmKeyVaultAccessPolicy -VaultName $keyVaultName -UserPrincipalName 'demodmo@outlook.fr' -PermissionsToKeys create,import,delete,list,get -PermissionsToSecrets set,delete,get
+}
+
+#Gestion du mot de passe avec notre container KeyVault
+{
+    $sqlAdminPassword = ConvertTo-SecureString -String $serverPassword -AsPlainText -Force
+
+    Set-AzureKeyVaultSecret -VaultName $keyVaultName -Name $keyVaultSqlAdminPasswordSecretName -SecretValue $sqlAdminPassword
+}
+
+
+#Déploiement du template dans le groupe de ressources
+
+{
+      New-AzureRmResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath -TemplateParameterFile $templateParametersFilePath -Verbose -Force
+
+}
